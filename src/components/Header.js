bb4e1d72f694762305e7032ccccc8da6772727bd
sjproctor <sproctor950@gmail.com>
2020-10-19 21:03:14 -0700

    adds file structure, routing, basic styling

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <h1>Cat Tinder</h1>
+      </React.Fragment>
+    )
+  }
+}
+export default Header
---
+++ src/pages/CatEdit.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class CatEdit extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <h3>Cat Edit</h3>
+      </React.Fragment>
+    )
+  }
+}
+export default CatEdit
