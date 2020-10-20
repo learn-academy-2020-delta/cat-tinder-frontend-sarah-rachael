44093169d1dd407730219526f17d5edb4097b470
sjproctor <sproctor950@gmail.com>
2020-10-20 12:10:13 -0700

    adds styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,44 +4,43 @@ import {
   Container,
   Jumbotron,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
+  NavbarToggler,
   Nav,
   NavItem,
   NavLink
 } from 'reactstrap'
 
 const Header = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
-
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
-  return (
-    <div>
+  const [collapsed, setCollapsed] = useState(true)
+  const toggleNavbar = () => setCollapsed(!collapsed)
+  return(
+    <React.Fragment>
       <Jumbotron fluid>
         <Container fluid>
           <h1 className="display-3">Cat Tinder</h1>
           <p className="lead">It's like Tinder but for cats.</p>
-          <Navbar color="faded" light>
-            <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-            <Collapse isOpen={!collapsed} navbar>
-              <Nav navbar>
-                <NavItem>
-                  <NavLink href="/catindex">See All the Cats</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/catnew">Create a New Cat</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/">Home</NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
+          <Navbar light>
+            <NavbarBrand href="/" className="mr-auto"></NavbarBrand>
+              <NavbarToggler onClick={ toggleNavbar } className="mr-2"/>
+              <Collapse isOpen={ !collapsed } navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink href="/catindex">See All the Cats</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/catnew">Create a New Cat</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/">Home</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
           </Navbar>
         </Container>
       </Jumbotron>
-    </div>
-  );
+    </React.Fragment>
+  )
 }
 
 export default Header
