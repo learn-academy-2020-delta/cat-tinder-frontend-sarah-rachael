1384d424195e244fc12d5d6628d49f8b498c20fa
sjproctor <sproctor950@gmail.com>
2020-10-21 13:58:54 -0700

    adds styling

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import { Redirect } from 'react-router-dom'
+import Emoji from 'a11y-react-emoji'
 
 class CatNew extends Component{
   constructor(props){
@@ -29,43 +30,53 @@ class CatNew extends Component{
   render(){
     return(
       <React.Fragment>
-        <h3>Cat New</h3>
-        <Form>
-          <FormGroup>
-            <Label for="name">Cat's Name</Label>
-            <Input
-              type="text"
-              name="name"
-              onChange={ this.handleChange }
-              value={ this.state.form.name }
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="age">Cat's Age</Label>
-            <Input
-              type="number"
-              name="age"
-              onChange={ this.handleChange }
-              value={ this.state.form.age }
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="enjoys">What does your cat enjoy?</Label>
-            <Input
-              type="text"
-              name="enjoys"
-              onChange={ this.handleChange }
-              value={ this.state.form.enjoys }
-            />
-          </FormGroup>
-          <Button
-            name="submit"
-            onClick={ this.handleSubmit }
-          >
-            Create New Cat
-          </Button>
-        </Form>
-        { this.state.success && <Redirect to="/catindex" /> }
+        <div id="new-body">
+          <h3 className="title">Join the Fun! <span><Emoji symbol="🐱" label="cat"/></span></h3>
+          <h4 className="title">Enter Your Cat's Information</h4>
+          <br />
+          <Form className="form">
+            <FormGroup>
+              <Label for="name">Cat's Name</Label>
+              <Input
+                type="text"
+                name="name"
+                className="form-input"
+                onChange={ this.handleChange }
+                value={ this.state.form.name }
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="age">Cat's Age</Label>
+              <Input
+                type="number"
+                name="age"
+                min="1"
+                max="30"
+                className="form-input"
+                onChange={ this.handleChange }
+                value={ this.state.form.age }
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="enjoys">What does your cat enjoy?</Label>
+              <Input
+                type="textarea"
+                name="enjoys"
+                className="form-input"
+                onChange={ this.handleChange }
+                value={ this.state.form.enjoys }
+              />
+            </FormGroup>
+            <Button
+              name="submit"
+              className="button"
+              onClick={ this.handleSubmit }
+            >
+              Create New Cat
+            </Button>
+          </Form>
+          { this.state.success && <Redirect to="/catindex" /> }
+        </div>
       </React.Fragment>
     )
   }
